d55e0246e4f552a44589e6ead0bf1f0f9fc7f91d
Paul Boeck <boeck87@gmail.com>
2019-02-11 21:42:35 +0100

    Add influxdb write

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # speedtest-influx
+
+
+## Local development
+
+For local development, start up an influxDB with
+
+```sh
+docker run --name=influxdb -p 8086:8086 -e INFLUXDB_DB=iot -v influxdb:/var/lib/influxdb influxdb
+```
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const http = require('http');
 const speedTest = require('speedtest-net');
 
 async function getCurrentSpeed() {
@@ -9,3 +10,32 @@ async function getCurrentSpeed() {
         test.on('error', reject);
     });
 }
+
+/**
+ * 
+ * @param {string} database 
+ * @param {string} message 
+ */
+async function sendToInflux(database, message) {
+    return new Promise((resolve, reject) => {
+        const buf = new Buffer(message.toString('binary'), 'binary');
+
+        const options = {
+            hostname: 'localhost',
+            port: 8086,
+            path: `/write?db=${database}`,
+            method: 'POST'
+        };
+
+        const req = http.request(options, res => {
+            res.on('end', resolve);
+        });
+        req.on('error', reject);
+        req.write(buf);
+        req.end();
+    });
+}
+
+const message = 'cpu_load_short,host=server01,region=us-west value=0.64 1434055562000000000';
+
+sendToInflux('iot', message);
